e9aaa851a7c6e7f7102b6919609b08535953c35c
Foxy224 <ppaaa4@gmail.com>
2023-06-15 13:20:43 +0300

    11

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,41 +1,27 @@
 package web.controller;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarServiceImlp;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import static web.service.CarServiceImlp.cars;
 
 
 @Controller
 public class CarController {
 
-    public static List<Car> cars = new ArrayList<>();
+    private final CarServiceImlp carServiceImlp;
 
-    static {
-        cars.add(new Car("vax", "lada", 227));
-        cars.add(new Car("las", "mazda", 222));
-        cars.add(new Car("gada", "laba", 222123));
-        cars.add(new Car("veslo", "mazda3", 224322));
-        cars.add(new Car("salo", "mazda7", 253622));
+    public CarController(CarServiceImlp carServiceImlp) {
+        this.carServiceImlp = carServiceImlp;
     }
-
-
 
     @GetMapping(value = "/cars")
     public String PrintCars(ModelMap model, @RequestParam (defaultValue = "5") int count) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan("web.service");
-        context.refresh();
-        CarServiceImlp carServiceImlp = context.getBean(CarServiceImlp.class);
         model.addAttribute("cars", carServiceImlp.ListCar(cars, count));
-        context.close();
         return "cars";
     }
 
--- src/main/java/web/service/CarServiceImlp.java
+++ src/main/java/web/service/CarServiceImlp.java
@@ -1,16 +1,28 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Service;
 import web.model.Car;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
 public class CarServiceImlp implements CarService{
 
-    @Override
+    public static  List<Car> cars = new ArrayList<>();
+
+     static {
+        cars.add(new Car("vax", "lada", 227));
+        cars.add(new Car("las", "mazda", 222));
+        cars.add(new Car("gada", "laba", 222123));
+        cars.add(new Car("veslo", "mazda3", 224322));
+        cars.add(new Car("salo", "mazda7", 253622));
+     }
+
+     @Override
     public List<Car> ListCar(List<Car> cars, int value) {
         if (value > cars.size()) {
             value = cars.size();
